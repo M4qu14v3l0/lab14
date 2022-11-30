c2dd0c7fe8959d63765cb60d8bb5a48ebe862f34
M4qu14v3l0 <marcelo.gabriel.llerena@gmail.com>
2022-11-30 11:59:32 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import Index from './pages/Index';
+import ForgotPassword from './pages/ForgotPassword'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import NotFound from './pages/NoFound';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
+
+function App() {
+  return (
+    
+    <BrowserRouter>
+    
+      <Routes>
+        <Route path="*" element={<NotFound/>} />
+        <Route path="/" element={<Index />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
